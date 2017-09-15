060871cd85a3210908c6e1789d0da3884e392459
aluno <aluno@MSCGRESL05019.estacio.acad>
2017-09-14 23:06:55 -0300

    Exercicio Testando Layout

--- src/br/estacio/programacao/exercicio/Exercicio.java
+++ src/br/estacio/programacao/exercicio/Exercicio.java
@@ -6,10 +6,11 @@
 package br.estacio.programacao.exercicio;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -23,18 +24,18 @@ import javax.swing.JTextField;
  * @author aluno
  */
 public class Exercicio extends JFrame {
-
+   
     private BorderLayout layout;
+
     private JPanel painelNorte;
     private JPanel painelSul;
     private JPanel painelCentro;
     private JPanel painelOeste;
     private JPanel painelLeste;
 
-    private BorderLayout borderLayoit;
+    private BorderLayout borderLayout;
     private FlowLayout flowLayout;
     private GridLayout gridLayout;
-    private BoxLayout boxLayout;
     private GridBagLayout gridBagLayout;
 
     private JLabel lblBorder1;
@@ -48,16 +49,18 @@ public class Exercicio extends JFrame {
     private JTextField txtGrid1;
     private JTextField txtGrid2;
     private JTextField txtGrid3;
-    
+
     private JRadioButton rbBox1;
-     private JRadioButton rbBox2;
-      private JRadioButton rbBox3;
-      
-      private JCheckBox cbGridBag1;
-      private JCheckBox cbGridBag2;
-      private JCheckBox cbGridBag3;
+    private JRadioButton rbBox2;
+    private JRadioButton rbBox3;
+
+    private JCheckBox cbGridBag1;
+    private JCheckBox cbGridBag2;
+    private JCheckBox cbGridBag3;
 
     public Exercicio() {
+        layout = new BorderLayout();
+
         painelCentro = new JPanel();
         painelNorte = new JPanel();
         painelSul = new JPanel();
@@ -65,30 +68,78 @@ public class Exercicio extends JFrame {
         painelOeste = new JPanel();
 
         flowLayout = new FlowLayout();
-        gridLayout = new GridLayout();
-        borderLayoit = new BorderLayout();
-        boxLayout = new BoxLayout(this, WIDTH);
+        gridLayout = new GridLayout(3, 1, 0, 0);
+        borderLayout = new BorderLayout();
         gridBagLayout = new GridBagLayout();
+
+        painelCentro.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        painelSul.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        painelLeste.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        painelNorte.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        painelOeste.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         lblBorder1 = new JLabel("Primeiro");
         lblBorder2 = new JLabel("Segundo");
         lblBorder3 = new JLabel("Terceiro");
+        lblBorder1.setBounds(5, 3, 50, 50);
+        lblBorder2.setBounds(5, 7, 70, 80);
+        lblBorder3.setBounds(5, 11, 100, 105);
 
         btnFlow1 = new JButton("Primeiro");
         btnFlow2 = new JButton("Segundo");
         btnFlow3 = new JButton("Terceiro");
-        
+
         txtGrid1 = new JTextField("Primeiro");
         txtGrid2 = new JTextField("Segundo");
         txtGrid3 = new JTextField("Terceiro");
-        
+
         rbBox1 = new JRadioButton("Primeiro");
         rbBox2 = new JRadioButton("Segundo");
         rbBox3 = new JRadioButton("Terceiro");
-        
+
         cbGridBag1 = new JCheckBox("Primeiro");
         cbGridBag2 = new JCheckBox("Segundo");
         cbGridBag3 = new JCheckBox("Terceiro");
+
+        painelCentro.setLayout(null);
+        painelSul.setLayout(gridLayout);
+        painelLeste.setLayout(borderLayout);
+        painelNorte.setLayout(flowLayout);
+        painelOeste.setLayout(gridBagLayout);
+
+        painelCentro.add(lblBorder1);
+        painelCentro.add(lblBorder2);
+        painelCentro.add(lblBorder3);
+
+        painelLeste.add(txtGrid1, BorderLayout.NORTH);
+        painelLeste.add(txtGrid2, BorderLayout.SOUTH);
+        painelLeste.add(txtGrid3, BorderLayout.CENTER);
+
+        painelNorte.add(rbBox1);
+        painelNorte.add(rbBox2);
+        painelNorte.add(rbBox3);
+
+        painelOeste.add(cbGridBag1);
+        painelOeste.add(cbGridBag2);
+        painelOeste.add(cbGridBag3);
+
+        painelSul.add(btnFlow1);
+        painelSul.add(btnFlow2);
+        painelSul.add(btnFlow3);
+
+        this.setLayout(layout);
+        this.add(painelCentro, BorderLayout.CENTER);
+        this.add(painelLeste, BorderLayout.WEST);
+        this.add(painelNorte, BorderLayout.NORTH);
+        this.add(painelOeste, BorderLayout.EAST);
+        this.add(painelSul, BorderLayout.SOUTH);
+
+        this.setSize(500, 520);
+        this.setVisible(true);
         
+         setLocationRelativeTo(null);
+         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+         
+
     }
 }
